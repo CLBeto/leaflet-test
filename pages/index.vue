cac2690f5d28d92949a4052ca3983400d0be0d68
CLBeto <humberto.l.calderon@aeterlink.com>
2024-10-29 14:20:08 +0900

    Sensor Data Mimic
    Added coordinates for sensor to display temperature heat data.

--- pages/index.vue
+++ pages/index.vue
@@ -19,9 +19,10 @@
     import { ref } from 'vue'
     import L from 'leaflet'
     import { LMap } from "@vue-leaflet/vue-leaflet";
+    import 'simpleheat'
 
     const map = ref(null) as any;
-    const zoom = ref(3)
+    const zoom = ref(4)
     const imageOverlayUrl = ref("/bm-canvas-image.png")
     const width = ref(100);
     const height = ref(100);
@@ -39,12 +40,12 @@
             leafletObject: map.value?.leafletObject,
             heatPoints: sensors.value,
             // (optional) radius : default 50
-            radius: 70
+            radius: 75
         });
         base.setOptions(
             {
                 gradient: gradient, 
-                blur: 56, 
+                blur: 55, 
                 pane: "shadowPane",
                 maxZoom: 3
             }
@@ -52,12 +53,12 @@
     }
 
     const gradient = {
-        0.1: '#011981', 
-        0.2: '#0041ff', 
-        0.3: '#0096ff',
+        // 0.1: '#011981', 
+        // 0.2: '#0041ff', 
+        0.1: '#0096ff',
         0.4: '#b9ebff',
         0.5: '#fffff0',
-        0.6: '#fffff0',
+        0.65: '#fffff0',
         0.7: '#fbf505',
         0.8: '#ff9900',
         0.9: '#ff2a02',
@@ -67,65 +68,37 @@
     // List of data provided by sensors, each object is a sensor.
     //lat = y(inverted), lng = x
     const sensors = ref([
-        {
-            lat: 71,
-            lng: 3,
-            intensity: .9
-        }, {
-            lat: 71,
-            lng: 19,
-            intensity: 0.7
-        }, {
-            lat: 71,
-            lng: 35,
-            intensity: 0.5
-        }, {
-            lat: 71,
-            lng: 49.5,
-            intensity: 0.3
-        }, {
-            lat: 71,
-            lng: 63.5,
-            intensity: 0.3
-        }, {
-            lat: 56,
-            lng: 3,
-            intensity: 0.7
-        }, {
-            lat: 56,
-            lng: 19,
-            intensity: 0.5
-        }, {
-            lat: 56,
-            lng: 35,
-            intensity: 0.3
-        }, {
-            lat: 56,
-            lng: 49.5,
+         {
+            lat: 49,
+            lng: 58,
             intensity: 1
         }, {
-            lat: 56,
-            lng: 63,
+            lat: 60,
+            lng: 61,
             intensity: 0.8
         }, {
-            lat: 41,
-            lng: 3,
+            lat: 45.5,
+            lng: 81,
+            intensity: 0.7
+        }, {
+            lat: 52.5,
+            lng: 83,
+            intensity: 0.7
+        },{
+            lat: 50,
+            lng: 90.5,
+            intensity: 0.7
+        }, {
+            lat: 56,
+            lng: 92.5,
+            intensity: 0.5
+        },{
+            lat: 71,
+            lng: 95.5,
             intensity: 0.5
         }, {
-            lat: 41,
-            lng: 19,
-            intensity: 0.5
-        }, {
-            lat: 41,
-            lng: 35,
-            intensity: 0.6
-        }, {
-            lat: 41,
-            lng: 49.5,
-            intensity: 0.4
-        }, {
-            lat: 41,
-            lng: 63,
+            lat: 34.5,
+            lng: 92.5,
             intensity: 0.3
         }
     ]);
